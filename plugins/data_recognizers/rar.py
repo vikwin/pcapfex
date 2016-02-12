bc3e5fcfeca3d88d90f5f3f7e705bd04c942e850
vikwin <djvw1983@googlemail.com>
2016-02-12 15:50:04 +0100

    added rar file recognizer

---
+++ plugins/data_recognizers/rar.py
@@ -0,0 +1,18 @@
+# -*- coding: utf8 -*-
+__author__ = 'Viktor Winkelmann'
+
+import sys
+
+sys.path.append('../..')
+from core.Plugins.DataRecognizer import *
+
+
+def getClassReference():
+    return RARFile
+
+
+class RARFile(SimpleDataRecognizer):
+    signatures = [(b'\x52\x61\x72\x21\x1A\x07', None)]
+    fileEnding = "rar"
+    dataType = "RAR file"
+    dataCategory = DataCategory.COMPRESSED
