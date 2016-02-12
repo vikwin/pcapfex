748e7fb15cb7b43cd47c25fc54c5c3bd0d51f413
vikwin <djvw1983@googlemail.com>
2016-02-12 15:27:36 +0100

    added gif file recognizer

---
+++ plugins/data_recognizers/gif.py
@@ -0,0 +1,18 @@
+# -*- coding: utf8 -*-
+__author__ = 'Viktor Winkelmann'
+
+import sys
+
+sys.path.append('../..')
+from core.Plugins.DataRecognizer import *
+
+
+def getClassReference():
+    return GIFFile
+
+
+class GIFFile(SimpleDataRecognizer):
+    signatures = [(b'\x47\x49\x46\x38[\x37\x39]\x61', b'\x00\x3B')]
+    fileEnding = "gif"
+    dataType = "GIF file"
+    dataCategory = DataCategory.IMAGE
